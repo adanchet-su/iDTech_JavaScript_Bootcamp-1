496facccf1531c5f49b3cc011d6ce7e25216e7d6
adanchet-su <alexinaancheta@gmail.com>
2022-01-22 14:48:35 +0800

    Story_Time

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,35 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+//4-strings
+let main_character = "Rose";
+let setting = "Visayas";
+let greetings = "Welcome to the Philippines!";
+let title = "The Story of the Filipino Farmer";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+//3-integers
+let year = 2021;
+let age = 27;
+let salary = 300;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+//1-array
+let crops = ['corn', 'rice', 'cacao', 'coconut', 'coffee beans'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+//1-boolean
+let sunny = true;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings + " Feel free to drink water and eat some snacks as we tour the area. My name is " + main_character + ", and I will be your tour guide for today. Let's go!");
+console.log("The theme of our tour today is " + title + ".");
+console.log("Here in " + setting + ", most of the people are either farmers or fishermen. I would like you to meet one of the farmers. His name is Caloy and he is " + age + " years old.");
+console.log("In previous years, farmers only use their hands with the help of carabaos to plant and harvest, but with the advanement of technology there are new equipment launched this " + year + ".");
+console.log("The most common crops found here is " + crops[0] + ".");
+console.log("Farmers work from sunrise to sunset, but only earn an average of " + salary + ". which is only equivalent to six dollars. With their salary, they pay for food, water, shelter, clothes, electricity, and tuition fee among others.");
+console.log("Our tour is about to come to an end. How are you feeling? It is hot, right? " + sunny + ".");
+console.log("Let's grab some halo-halo then! Thank you so much for participating. Next time I'll show you the waterfalls.")
